b7628832d95a4087241a22dd90809ec76780b668
Shreyas Challa <shreyas.challa3@gmail.com>
2026-01-21 13:08:47 -0500

    indexing for multiple files in notes folder

--- indexer.py
+++ indexer.py
@@ -1,40 +1,45 @@
+import os
 import re
 import ftfy
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-path = "./Lecture 15- Dijkstra's algorithm.md"
+PATH_TO_NOTES_FOLDER = "./files/"
+data = [] # corpus to analyse and transform for fit and search
 
-pattern = r"\[\[(.*?)\]\]" # turning note links back into normal text
-replacement = r"\1"
 
-data = []
+for filename in os.listdir(PATH_TO_NOTES_FOLDER):
+    if filename.endswith(".md"): # Filter for mark down files
+        path = os.path.join(PATH_TO_NOTES_FOLDER, filename)
+        with open(path, 'r', encoding="utf-8") as f:
+            file = f.read()
+            fixed_text = ftfy.fix_text(file) #clean the unicode transformation errors
 
-with open(path, 'r') as f:
-  file = f.read()
-  fixed_text = ftfy.fix_text(file) #clean the unicode transformation errors
-  
-  text = re.sub(pattern, replacement, fixed_text, flags=re.MULTILINE) # replace note links with inner text
+            pattern = r"\[\[(.*?)\]\]" # pattern for note links
+            replacement = r"\1" # use inner text
+            text = re.sub(pattern, replacement, fixed_text, flags=re.MULTILINE) # replace note links with inner text
 
-  pattern = r'^!(?:\[\[|Pasted).*\n?' # regex pattern for images
-  cleaned_text = re.sub(pattern, '', text, flags=re.MULTILINE)
-  
-  pattern = r'```.*?```'
-  not_final = re.sub(pattern, '', cleaned_text, flags=re.DOTALL) # delete the code blocks from existence
+            pattern = r'^!(?:\[\[|Pasted).*\n?' # regex pattern for images
+            cleaned_text = re.sub(pattern, '', text, flags=re.MULTILINE)
+            
+            pattern = r'```.*?```'
+            not_final = re.sub(pattern, '', cleaned_text, flags=re.DOTALL) # delete the code blocks from existence
 
-  pattern = r"`([^`]*)`"
-  replacement = r"\1"
-  final = re.sub(pattern, replacement, not_final, flags=re.MULTILINE) # replace the fucking inline shit
+            pattern = r"`([^`]*)`"
+            replacement = r"\1"
+            final = re.sub(pattern, replacement, not_final, flags=re.MULTILINE) # replace the fucking inline shit
 
-  words = final.split()
-  
-  # data.append(words)
-  # print(final)
-  # print(words)
+            words = final.split()
 
+            # print(final)
+            # print(words)
+            data.append(final) # append to corpus
+            
+print(data[0])
 
 tfidf = TfidfVectorizer()
-result = tfidf.fit_transform(words)
+result = tfidf.fit_transform(data)
+print(result.shape)
 
 for ele1, ele2 in zip(tfidf.get_feature_names_out(), tfidf.idf_):
     print(ele1, ':', ele2)
